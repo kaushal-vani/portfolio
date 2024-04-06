9b8132dd9ecd2d44749021363bc71ed9910df041
kaushal <kaushal2469@gmail.com>
2024-04-06 12:33:49 +0530

    Update Ui index.ts

--- ng-portfolio/projects/portfolio-ui/src/index.ts
+++ ng-portfolio/projects/portfolio-ui/src/index.ts
@@ -1,7 +1,9 @@
 // Public API Surface of portfolio-ui
 
 //Features
-export * from './lib/features/hero-banner/hero-banner.component'
+export * from './lib/features/hero-banner/hero-banner.component';
 
 //Pages
-export * from './lib/pages/home-page/home-page.component'
+export * from './lib/pages/about-page/about-page.component';
+export * from './lib/pages/display-page/display-page.component';
+export * from './lib/pages/home-page/home-page.component';
