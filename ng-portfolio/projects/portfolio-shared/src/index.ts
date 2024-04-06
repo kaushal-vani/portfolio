6e9ec2b46301b65bb6e44421eb9bfec5d1a92c9a
kaushal <kaushal2469@gmail.com>
2024-04-06 23:55:01 +0530

    export shared interface in index.ts

--- ng-portfolio/projects/portfolio-shared/src/index.ts
+++ ng-portfolio/projects/portfolio-shared/src/index.ts
@@ -6,3 +6,6 @@ export * from './lib/components/footer/footer.component';
 export * from './lib/components/global-nav/global-nav.component';
 export * from './lib/components/secondary-nav/secondary-nav.component';
 export * from './lib/components/home-skeleton-loader/home-skeleton-loader.component';
+
+// Models
+export * from './lib/models/shared.interface';
