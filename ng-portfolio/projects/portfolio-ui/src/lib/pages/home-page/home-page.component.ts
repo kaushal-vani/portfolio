492df61d8e5765b4bfbaaad54c8e8947e37dc0a0
kaushal <kaushal2469@gmail.com>
2024-04-06 11:41:49 +0530

    integrate features and pages

--- ng-portfolio/projects/portfolio-ui/src/lib/pages/home-page/home-page.component.ts
+++ ng-portfolio/projects/portfolio-ui/src/lib/pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroBannerComponent } from 'portfolio-ui/src';
 
 @Component({
-  selector: 'portfolio-home-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+    selector: 'portfolio-home-page',
+    standalone: true,
+    templateUrl: './home-page.component.html',
+    styleUrl: './home-page.component.css',
+    imports: [HeroBannerComponent]
 })
 export class HomePageComponent {
 
