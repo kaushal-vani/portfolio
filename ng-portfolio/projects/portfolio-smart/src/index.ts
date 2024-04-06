1172c94bf505d4d3b12613f980a33a29bd41a001
kaushal <kaushal2469@gmail.com>
2024-04-06 12:39:05 +0530

    Update Smart index.ts

--- ng-portfolio/projects/portfolio-smart/src/index.ts
+++ ng-portfolio/projects/portfolio-smart/src/index.ts
@@ -1,6 +1,8 @@
 // Public API Surface of portfolio-smart
 
 //Entry-Components
-export * from './lib/entry-components/home/home.component'
+export * from './lib/entry-components/about/about.component';
+export * from './lib/entry-components/display/display.component';
+export * from './lib/entry-components/home/home.component';
 
 //Data-Access
