b5405757d0cdeb1cf31c4ede429cf70361bde2c1
kaushal <kaushal2469@gmail.com>
2024-04-06 11:31:23 +0530

    Re-structure portfolio-ui library

---
+++ ng-portfolio/projects/portfolio-ui/src/index.ts
@@ -0,0 +1,7 @@
+// Public API Surface of portfolio-ui
+
+//Features
+export * from './lib/features/hero-banner/hero-banner.component'
+
+//Pages
+export * from './lib/pages/home-page/home-page.component'
--- ng-portfolio/projects/portfolio-ui/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of portfolio-ui
- */
-
-export * from './lib/portfolio-ui.service';
-export * from './lib/portfolio-ui.component';
