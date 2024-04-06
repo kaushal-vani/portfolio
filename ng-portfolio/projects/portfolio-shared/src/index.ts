f09a0d9d0363a6c863469e572d5c6678f1d1aefa
kaushal <kaushal2469@gmail.com>
2024-04-06 12:23:50 +0530

    Update Shared index.ts

--- ng-portfolio/projects/portfolio-shared/src/index.ts
+++ ng-portfolio/projects/portfolio-shared/src/index.ts
@@ -1,4 +1,7 @@
 // Public API Surface of  portfolio-shared
 
 // Components
-export * from './lib/card/card.component'
+export * from './lib/components/card/card.component';
+export * from './lib/components/footer/footer.component';
+export * from './lib/components/global-nav/global-nav.component';
+export * from './lib/components/secondary-nav/secondary-nav.component';
