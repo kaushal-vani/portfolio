975d1233380ef16fc9d663addce2a3430ca4bd3e
kaushal <kaushal2469@gmail.com>
2024-04-06 12:57:53 +0530

    configure application routing

--- ng-portfolio/projects/portfolio/src/app/app.routes.ts
+++ ng-portfolio/projects/portfolio/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import {
+  AboutComponent,
+  DisplayComponent,
+  HomeComponent,
+} from 'portfolio-smart/src';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'display',
+    component: DisplayComponent,
+  },
+  {
+    path: '**',
+    component: HomeComponent,
+  },
+];
