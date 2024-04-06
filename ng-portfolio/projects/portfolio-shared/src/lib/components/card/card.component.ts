162eb5b323f845b0fb29c6fb3d0456251f971b52
kaushal <kaushal2469@gmail.com>
2024-04-06 12:14:02 +0530

    Create shared-card component

---
+++ ng-portfolio/projects/portfolio-shared/src/lib/components/card/card.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+  @Input() imageUrl: string = '';
+  @Input() altText: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() meta: string = '';
+}
