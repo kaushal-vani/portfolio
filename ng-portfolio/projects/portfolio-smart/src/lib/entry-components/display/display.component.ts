a64475383d35ff5bbe5f1f3f11c890efc835571c
kaushal <kaushal2469@gmail.com>
2024-04-06 12:42:22 +0530

    integrate display page with smart

--- ng-portfolio/projects/portfolio-smart/src/lib/entry-components/display/display.component.ts
+++ ng-portfolio/projects/portfolio-smart/src/lib/entry-components/display/display.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DisplayPageComponent } from 'portfolio-ui/src';
 
 @Component({
-  selector: 'portfolio-display',
-  standalone: true,
-  imports: [],
-  templateUrl: './display.component.html',
-  styleUrl: './display.component.css'
+    selector: 'portfolio-display',
+    standalone: true,
+    templateUrl: './display.component.html',
+    styleUrl: './display.component.css',
+    imports: [DisplayPageComponent]
 })
 export class DisplayComponent {
 
