b3cadb81a3da2148a12393738d8c95b44e0a639c
kaushal <kaushal2469@gmail.com>
2024-04-06 12:33:06 +0530

    create display component

---
+++ ng-portfolio/projects/portfolio-ui/src/lib/pages/display-page/display-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-display-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-page.component.html',
+  styleUrl: './display-page.component.css'
+})
+export class DisplayPageComponent {
+
+}
