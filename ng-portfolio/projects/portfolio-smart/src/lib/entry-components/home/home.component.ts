0ff08365c6d8aee87735ff34d59507bdfa472b7e
kaushal <kaushal2469@gmail.com>
2024-04-06 11:50:50 +0530

    integrate pages and entry-component

--- ng-portfolio/projects/portfolio-smart/src/lib/entry-components/home/home.component.ts
+++ ng-portfolio/projects/portfolio-smart/src/lib/entry-components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { HomePageComponent } from 'portfolio-ui/src';
 
 @Component({
-  selector: 'portfolio-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'portfolio-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [HomePageComponent]
 })
 export class HomeComponent {
 
