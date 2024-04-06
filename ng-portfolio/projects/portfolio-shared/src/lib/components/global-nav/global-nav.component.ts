12ebe997e32b81f3510567ba558bb494c9ba7d1b
kaushal <kaushal2469@gmail.com>
2024-04-06 12:22:34 +0530

    Create shared-global-nav component

---
+++ ng-portfolio/projects/portfolio-shared/src/lib/components/global-nav/global-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-global-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './global-nav.component.html',
+  styleUrl: './global-nav.component.css'
+})
+export class GlobalNavComponent {
+
+}
