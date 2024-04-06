8a22af21dad7325cd6b7302f6e0d9606d50199bc
kaushal <kaushal2469@gmail.com>
2024-04-06 11:53:04 +0530

    integrate entry-component and application

--- ng-portfolio/projects/portfolio/src/app/app.component.ts
+++ ng-portfolio/projects/portfolio/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from 'portfolio-smart/src';
 
 @Component({
-  selector: 'portfolio-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'portfolio-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'portfolio';
