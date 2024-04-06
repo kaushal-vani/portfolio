56667f30617cc91a0a43765d8dde12bbcf88543e
kaushal <kaushal2469@gmail.com>
2024-04-06 12:42:03 +0530

    integrate about page with smart

--- ng-portfolio/projects/portfolio-smart/src/lib/entry-components/about/about.component.ts
+++ ng-portfolio/projects/portfolio-smart/src/lib/entry-components/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AboutPageComponent } from 'portfolio-ui/src';
 
 @Component({
-  selector: 'portfolio-about',
-  standalone: true,
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+    selector: 'portfolio-about',
+    standalone: true,
+    templateUrl: './about.component.html',
+    styleUrl: './about.component.css',
+    imports: [AboutPageComponent]
 })
 export class AboutComponent {
 
