b47c109fd5a61c404832c5c90d0ff60d5baa2efb
kaushal <kaushal2469@gmail.com>
2024-04-06 11:47:13 +0530

    Restructure portflio-smart library

---
+++ ng-portfolio/projects/portfolio-smart/src/index.ts
@@ -0,0 +1,6 @@
+// Public API Surface of portfolio-smart
+
+//Entry-Components
+export * from './lib/entry-components/home/home.component'
+
+//Data-Access
--- ng-portfolio/projects/portfolio-smart/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of portfolio-smart
- */
-
-export * from './lib/portfolio-smart.service';
-export * from './lib/portfolio-smart.component';
