7c8017f2db02b5e0de526bfeb37eb3268c4892e1
kaushal <kaushal2469@gmail.com>
2024-04-06 12:13:17 +0530

    Re-structure portfolio-shared library

---
+++ ng-portfolio/projects/portfolio-shared/src/index.ts
@@ -0,0 +1,4 @@
+// Public API Surface of  portfolio-shared
+
+// Components
+export * from './lib/card/card.component'
--- ng-portfolio/projects/portfolio-shared/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of portfolio-shared
- */
-
-export * from './lib/portfolio-shared.service';
-export * from './lib/portfolio-shared.component';
