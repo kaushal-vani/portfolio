5e43e14c12146ecb2acbd013f86764875b15827e
kaushal <kaushal2469@gmail.com>
2024-04-07 20:48:22 +0530

    consume logo carousel in display component

--- ng-portfolio/projects/portfolio-ui/src/lib/pages/display-page/display-page.component.ts
+++ ng-portfolio/projects/portfolio-ui/src/lib/pages/display-page/display-page.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ProjectContent } from './models/display-page.interface';
 import { ProjectContentMock } from './mocks/display-page.mocks';
+import { LogoCarouselComponent } from 'portfolio-ui/src';
 
 @Component({
-  selector: 'portfolio-display-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './display-page.component.html',
-  styleUrl: './display-page.component.scss',
+    selector: 'portfolio-display-page',
+    standalone: true,
+    templateUrl: './display-page.component.html',
+    styleUrl: './display-page.component.scss',
+    imports: [LogoCarouselComponent]
 })
 export class DisplayPageComponent {
   content: ProjectContent = ProjectContentMock;
