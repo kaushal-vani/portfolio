ea74373a3a3233346ea00344efc46edc8336044e
kaushal <kaushal2469@gmail.com>
2024-04-08 00:43:54 +0530

    download resume on button click implementation

--- ng-portfolio/projects/portfolio-ui/src/lib/features/hero-banner/hero-banner.component.ts
+++ ng-portfolio/projects/portfolio-ui/src/lib/features/hero-banner/hero-banner.component.ts
@@ -8,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './hero-banner.component.scss'
 })
 export class HeroBannerComponent {
+  downloadResume() {
+    // Replace 'path/to/your/resume.pdf' with the actual path to your resume file
+    const resumeUrl = 'assets/Resume/dummy.pdf';
+
+    // Create a temporary anchor element
+    const link = document.createElement('a');
+    link.href = resumeUrl;
+
+    // Set the download attribute and filename
+    link.download = 'kaushal-resume.pdf';
+
+    // Trigger the download by clicking the anchor element programmatically
+    document.body.appendChild(link);
+    link.click();
+
+    // Clean up: remove the anchor element from the DOM
+    document.body.removeChild(link);
+  }
 
 }
