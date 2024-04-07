7bc1d5e362b3023d5b858e34d94b0196beab319a
kaushal <kaushal2469@gmail.com>
2024-04-07 15:38:37 +0530

    create email me component

---
+++ ng-portfolio/projects/portfolio-shared/src/lib/components/email-form/email-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-email-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './email-form.component.html',
+  styleUrl: './email-form.component.scss'
+})
+export class EmailFormComponent {
+
+}
