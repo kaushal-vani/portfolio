0d21a213a27f08dad4375754b1c00308cad81470
kaushal <kaushal2469@gmail.com>
2024-04-07 15:39:26 +0530

    add emal me component in index.ts

--- ng-portfolio/projects/portfolio-shared/src/index.ts
+++ ng-portfolio/projects/portfolio-shared/src/index.ts
@@ -2,6 +2,7 @@
 
 // Components
 export * from './lib/components/card/card.component';
+export * from './lib/components/email-form/email-form.component'
 export * from './lib/components/footer/footer.component';
 export * from './lib/components/global-nav/global-nav.component';
 export * from './lib/components/secondary-nav/secondary-nav.component';
