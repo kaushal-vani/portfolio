9a942b8ec502a4f30cf29ab0da1c9f05cb06e84b
kaushal <kaushal2469@gmail.com>
2024-04-07 14:19:39 +0530

    update smart and ui index.ts

--- ng-portfolio/projects/portfolio-ui/src/index.ts
+++ ng-portfolio/projects/portfolio-ui/src/index.ts
@@ -1,9 +1,11 @@
 // Public API Surface of portfolio-ui
 
 //Features
+export * from './lib/features/contact-me/contact-me.component';
 export * from './lib/features/hero-banner/hero-banner.component';
 
 //Pages
 export * from './lib/pages/about-page/about-page.component';
+export * from './lib/pages/contact-page/contact-page.component';
 export * from './lib/pages/display-page/display-page.component';
 export * from './lib/pages/home-page/home-page.component';
