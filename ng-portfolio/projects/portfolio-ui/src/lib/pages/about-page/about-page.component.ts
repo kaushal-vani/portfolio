3b91bc466052d253c45451fc04d0d3e003cc5068
kaushal <kaushal2469@gmail.com>
2024-04-13 14:21:16 +0530

    consume instagram component in my about page

--- ng-portfolio/projects/portfolio-ui/src/lib/pages/about-page/about-page.component.ts
+++ ng-portfolio/projects/portfolio-ui/src/lib/pages/about-page/about-page.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { CardComponent } from 'portfolio-shared/src';
 import { AboutPageContentMock } from './mocks/about-page.mocks';
 import { AboutPageContent } from './models/about-page.interface';
+import { InstagramComponent } from 'portfolio-ui/src';
 
 @Component({
   selector: 'portfolio-about-page',
   standalone: true,
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss',
-  imports: [CardComponent],
+  imports: [CardComponent, InstagramComponent],
 })
 export class AboutPageComponent {
-  content: AboutPageContent = AboutPageContentMock
-
+  content: AboutPageContent = AboutPageContentMock;
 }
