1c50153222ec5c0762292f0cf04f70e92f99f8e8
kaushal <kaushal2469@gmail.com>
2024-04-13 14:18:28 +0530

    update index ts

--- ng-portfolio/projects/portfolio-shared/src/index.ts
+++ ng-portfolio/projects/portfolio-shared/src/index.ts
@@ -2,11 +2,15 @@
 
 // Components
 export * from './lib/components/card/card.component';
-export * from './lib/components/email-form/email-form.component'
+export * from './lib/components/email-form/email-form.component';
 export * from './lib/components/footer/footer.component';
 export * from './lib/components/global-nav/global-nav.component';
-export * from './lib/components/secondary-nav/secondary-nav.component';
 export * from './lib/components/home-skeleton-loader/home-skeleton-loader.component';
+export * from './lib/components/secondary-nav/secondary-nav.component';
 
 // Models
 export * from './lib/models/shared.interface';
+
+//Services
+export * from './lib/services/download-resume.service';
+export * from './lib/services/instagram.service';
--- ng-portfolio/projects/portfolio-ui/src/index.ts
+++ ng-portfolio/projects/portfolio-ui/src/index.ts
@@ -3,6 +3,7 @@
 //Features
 export * from './lib/features/contact-me/contact-me.component';
 export * from './lib/features/hero-banner/hero-banner.component';
+export * from './lib/features/instagram/instagram.component';
 export * from './lib/features/logo-carousel/logo-carousel.component';
 
 //Pages
