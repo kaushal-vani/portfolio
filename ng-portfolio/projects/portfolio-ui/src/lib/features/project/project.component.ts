ef622e3c1c78fdbbbfc0273e5f902a1dff425db4
kaushal <kaushal2469@gmail.com>
2024-04-14 16:11:17 +0530

    create project component

---
+++ ng-portfolio/projects/portfolio-ui/src/lib/features/project/project.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-project',
+  standalone: true,
+  imports: [],
+  templateUrl: './project.component.html',
+  styleUrl: './project.component.scss'
+})
+export class ProjectComponent {
+
+}
