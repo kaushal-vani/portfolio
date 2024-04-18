bab63cdc7991f4a747d232ff9db5cdf7432f2c87
kaushal <kaushal2469@gmail.com>
2024-04-18 12:16:17 +0530

    integrate project component in display page

--- ng-portfolio/projects/portfolio-ui/src/lib/pages/display-page/display-page.component.ts
+++ ng-portfolio/projects/portfolio-ui/src/lib/pages/display-page/display-page.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { ProjectContent } from './models/display-page.interface';
 import { ProjectContentMock } from './mocks/display-page.mocks';
 import { LogoCarouselComponent } from 'portfolio-ui/src';
+import { ProjectComponent } from "../../features/project/project.component";
 
 @Component({
     selector: 'portfolio-display-page',
     standalone: true,
     templateUrl: './display-page.component.html',
     styleUrl: './display-page.component.scss',
-    imports: [LogoCarouselComponent]
+    imports: [LogoCarouselComponent, ProjectComponent]
 })
 export class DisplayPageComponent {
   content: ProjectContent = ProjectContentMock;
