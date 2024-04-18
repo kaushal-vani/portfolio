46f64d15a2bcedbd496fe1642a9a46ec0dd100d1
kaushal <kaushal2469@gmail.com>
2024-04-18 17:57:34 +0530

    re-organise features

--- ng-portfolio/projects/portfolio-ui/src/lib/pages/tech-stack-page/tech-stack-page.component.ts
+++ ng-portfolio/projects/portfolio-ui/src/lib/pages/tech-stack-page/tech-stack-page.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { TechStackContentMocks } from './mocks/tech-stack-page.mocks';
 import { TechStackContent } from './models/tech-stack-page.interface';
 import { CardComponent } from 'portfolio-shared/src';
+import { LogoCarouselComponent } from 'portfolio-ui/src';
 
 @Component({
-    selector: 'portfolio-tech-stack-page',
-    standalone: true,
-    templateUrl: './tech-stack-page.component.html',
-    styleUrl: './tech-stack-page.component.scss',
-    imports: [CardComponent]
+  selector: 'portfolio-tech-stack-page',
+  standalone: true,
+  templateUrl: './tech-stack-page.component.html',
+  styleUrl: './tech-stack-page.component.scss',
+  imports: [CardComponent, LogoCarouselComponent],
 })
 export class TechStackPageComponent {
   content = TechStackContentMocks as TechStackContent;
