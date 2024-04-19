d16f04ce2fac7778abb1c119ca8ce4ad6d98454f
kaushal <kaushal2469@gmail.com>
2024-04-19 09:27:36 +0530

    update and consume current view feature in project overview component

---
+++ ng-portfolio/projects/portfolio-ui/src/lib/pages/project-overview/project-overview.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CurrentViewService, CURRENT_VIEW } from 'portfolio-shared/src';
+
+@Component({
+  selector: 'portfolio-project-overview',
+  templateUrl: './project-overview.component.html',
+  styleUrls: ['./project-overview.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class ProjectOverviewComponent implements OnInit, OnDestroy {
+  currentView = '';
+  CURRENT_VIEW = CURRENT_VIEW;
+
+  constructor(private currentViewService: CurrentViewService) {}
+
+  ngOnInit(): void {
+    this.currentView = this.currentViewService.getCurrentView();
+    console.log('get', this.currentView);
+  }
+  ngOnDestroy() {}
+}
